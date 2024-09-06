a92d29be891576806744cb8563e4ad668652521b
Pintu Kumar Giri <131274412+Pintu95045@users.noreply.github.com>
2024-09-06 21:57:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 //your JS code here. If required.
-const h = document.querySelector('h1');
-
-function updateSize() {
-	let width = window.innerWidth;
-	let height = window.innerHeight;
-	h.innerText = `Width: ${width} and Height: ${height}`;
+function getSize()
+{
+var w = document.documentElement.clientWidth;
+var h = document.documentElement.clientHeight;
+        
+// put the result into a h1 tag
+ document.getElementById('wh').innerHTML = "<h1>Width: " + w + " â€¢ Height: " + h + "</h1>";
 }
-updateSize();
-window.addEventListener('resize', updateSize);
