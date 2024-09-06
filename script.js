aebdc52c57428f51c4dc0caaf90ef352d01f5b6b
Pintu Kumar Giri <131274412+Pintu95045@users.noreply.github.com>
2024-09-06 21:59:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 //your JS code here. If required.
-function getSize()
-{
-var w = document.documentElement.clientWidth;
-var h = document.documentElement.clientHeight;
-        
-// put the result into a h1 tag
- document.getElementById('wh').innerHTML = "<h1>Width: " + w + " â€¢ Height: " + h + "</h1>";
-}
+let height=this.innerHeight;
+    let width=this.innerWidth;
+   let div=document.getElementById("sizeInfo");
+    let h=div.firstChild;
+    h.innerText="Width: "+width+" and Height: "+height;  
+window.addEventListener("resize",function(){
+    let height=this.innerHeight;
+    let width=this.innerWidth;
+   let div=document.getElementById("sizeInfo");
+    let h=div.firstChild;
+ 
+    h.innerText="Width: "+width+" and Height: "+height; 
+  
+  })
